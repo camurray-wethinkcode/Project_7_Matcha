19cbdb457448f20dc4317f0d3dcc8f18b5037110
Catherine <50704195+camurray-wethinkcode@users.noreply.github.com>
2020-06-29 10:56:49 +0200

    Added other gender to search
    Added other gender to the search frontend and backend

--- Matcha.API/Controllers/UsersController.cs
+++ Matcha.API/Controllers/UsersController.cs
@@ -42,7 +42,10 @@ namespace Matcha.API.Controllers
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 Console.WriteLine(gender);
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -61,9 +64,13 @@ namespace Matcha.API.Controllers
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 Console.WriteLine(gender);
                 userParams.Gender = gender;
                 Console.WriteLine(gender);
+=======
+                userParams.Gender = gender;
+>>>>>>> Stashed changes
 =======
                 userParams.Gender = gender;
 >>>>>>> Stashed changes
