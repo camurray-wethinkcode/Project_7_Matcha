fe514eeb1d95a653dbc76a8e2ec231855b797307
Catherine <50704195+camurray-wethinkcode@users.noreply.github.com>
2020-07-06 00:27:20 +0200

    Added a toggle on registration
    Added a toggle for the registration button on the homepage to make it invisible if a user is logged in

--- Matcha-SPA/src/app/home/home.component.ts
+++ Matcha-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  isShow = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.authService.loggedIn() === true)
+      this.isShow = !this.isShow;
+}
 
   registerToggle() {
     this.registerMode = true;
