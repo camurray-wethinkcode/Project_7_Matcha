b67386179fc075acf2aaeb0c492f0bc65987f315
░▒▓█│【Walkman】│█▓▒░ <matthew.carter@structureit.net>
2020-07-10 23:27:18 +0200

    actually added file

---
+++ Matcha.API/Dtos/UserForResetSendDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matcha.API.Dtos
+{
+    public class UserForResetSendDto
+    {
+        public string Email { get; set; }
+    }
+}
