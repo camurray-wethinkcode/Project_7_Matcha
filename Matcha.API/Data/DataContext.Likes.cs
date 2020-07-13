455503bafde67a03275d700aa37ba8a341edf0c5
░▒▓█│【Walkman】│█▓▒░ <matthew.carter@structureit.net>
2020-07-13 23:08:58 +0200

    added Likes DataContext

---
+++ Matcha.API/Data/DataContext.Likes.cs
@@ -0,0 +1,60 @@
+using System.Threading.Tasks;
+using Matcha.API.Models;
+
+namespace Matcha.API.Data
+{
+    public interface ILikesDataContext
+    {
+        public Task<Like> Get(int likerId, int likeeId);
+        public Task<bool> Add(Like like);
+        public Task<bool> Delete(int likerId, int likeeId);
+    }
+
+    public class LikesDataContext : ILikesDataContext
+    {
+        private readonly IDbAccess _dbAccess;
+
+        public LikesDataContext(IDbAccess dbAccess)
+        {
+            _dbAccess = dbAccess;
+        }
+
+        private const string _likesDBValues = "`LikerId`, `LikeeId`";
+
+        public async Task<Like> Get(int likerId, int likeeId)
+        {
+            var values = await _dbAccess.SelectOne("SELECT" + _likesDBValues +
+                "FROM `Likes`" +
+                "WHERE" +
+                "   `LikerId` = @LikerId" +
+                "AND" +
+                "   `LikeeId` = @LikeeId",
+                new DBParam("LikerId", likerId),
+                new DBParam("LikeeId", likeeId));
+
+            return new Like
+            {
+                LikerId = (int)values[0],
+                LikeeId = (int)values[1]
+            };
+        }
+
+        public async Task<bool> Add(Like like)
+        {
+            var updateAmount = await _dbAccess.Insert("INSERT INTO `Likes` (" + _likesDBValues + ") VALUES (@LikerId, @LikeeId)",
+                new DBParam("LikerId", like.LikerId), new DBParam("LikeeId", like.LikeeId));
+
+            return updateAmount == 1;
+        }
+
+        public async Task<bool> Delete(int likerId, int likeeId)
+        {
+            return await _dbAccess.Delete("DELETE FROM `Likes` WHERE" +
+                "   `LikerId` = @LikerId" +
+                "AND" +
+                "   `LikeeId` = @LikeeId",
+                new DBParam("LikerId", likerId),
+                new DBParam("LikeeId", likeeId));
+        }
+    }
+}
--- Matcha.API/Data/DataContext.User.cs
+++ Matcha.API/Data/DataContext.User.cs
@@ -1,4 +1,3 @@
-using System.Data.Common;
 using System.Threading.Tasks;
 using Matcha.API.Models;
 
